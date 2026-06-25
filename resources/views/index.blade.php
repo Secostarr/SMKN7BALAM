ae9d591bc5ad95bfc4e2a72cae0636dc26d4ed05
Secostarr <bintangdiks676@gamil.com>
2026-06-25 08:30:34 +0700

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,11 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMKN 7 Bandar Lampung</title>
 
-    <!-- Plus Jakarta Sans - Font modern, bersih, dan rapi untuk menunjukkan institusi yang maju dan profesional -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
-    
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
